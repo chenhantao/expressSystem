391c7a37cbec8b4d2fe576b08abdd451b2ba3508
chenhantao <372101345@qq.com>
2022-10-08 02:45:50 +0800

    add

---
+++ expressSystem-web/src/main/java/com/cht/Application.java
@@ -0,0 +1,17 @@
+package com.cht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类还有的说么
+ *
+ * @author chenhantao
+ * @since 2019/12/21
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
